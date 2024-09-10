03e216dfb85c690eda9761b6cc2a2e11bf1a1c40
Sunil Shrestha <157888618+sunilshrestha07@users.noreply.github.com>
2024-09-10 17:08:50 +0545

    Update projectsCluster.ts

--- src/projectsCluster.ts
+++ src/projectsCluster.ts
@@ -10,20 +10,20 @@ interface ProjectsInterface {
 
 export const projects: ProjectsInterface[] = [
     {
-        name : "Moodlift",
-        description : "MoodLift is an AI-powered mental health platform that provides personalized well-being suggestions. After users answer initial questions, the AI analyzes the inputs to offer tailored advice on improving mental health. Additionally, users can connect with certified professionals for further guidance, making it a comprehensive resource for mental health support. ",
+        name : "FashionFusion",
+        description : "FashionFusion is my e-commerce full stack project build in Next js, it provides a seamless shopping experience for users. It offers a user-friendly interface, secure logins, and shopping cart functionality and for the payment i have intergrated E-sewa payment gateway. ",
         image:"/images/fashion.png",
-        codeLink:"https://github.com/sunilshrestha07/moodlift",
+        codeLink:"https://github.com/sunilshrestha07/FashionFusion",
         liveLink: "https://fashion-fusion-suneel.vercel.app/",
         codeLinkImage : "/images/github.png",
         liveLinkImage : "/images/share.png",
     },
     {
-        name : "Hantay",
-        description : "Hantay, a food ordering website, lets you explore  order delicious meals with ease. Built with the MERN stack (MongoDB, Express, React, Node.js) and Tailwind CSS, it offers secure logins (Firebase), a user-friendly interface" ,
-        image:"/images/hantay.jpg",
-        codeLink:"https://github.com/sunilshrestha07/Hantay",
-        liveLink: "https://moodlift.vercel.app/",
+        name : "Moodlift",
+        description : "MoodLift is an AI-powered mental health platform that provides personalized well-being suggestions. After users answer initial questions, the AI analyzes the inputs to offer tailored advice on improving mental health. Additionally, users can connect with certified professionals for further guidance, making it a comprehensive resource for mental health support. ",
+        image:"/images/fashion.png",
+        codeLink:"https://github.com/sunilshrestha07/moodlift",
+        liveLink: "https://fashion-fusion-suneel.vercel.app/",
         codeLinkImage : "/images/github.png",
         liveLinkImage : "/images/share.png",
     },
